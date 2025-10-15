b39d6bc803634e62cd33fbf9950e456fb05a21c1
Carlos-RT <carlos.ramirez02@unicatolica.edu.co>
2025-10-15 03:08:24 -0500

    Refactorizacion reviada y finalizada por Carlos

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  js.configs.recommended,
+  prettierConfig,
+  {
+
+    languageOptions: {
+      globals: {
+        module: "readonly",
+        require: "readonly",
+      },
+    },
+
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": ["error"],
+    },
+  },
+];
